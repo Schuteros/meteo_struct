a1f6c1f4ca41d94b234d62a960ffae87adbd3330
Schuteros <kurpnieksuldis@gmail.com>
2023-04-20 22:34:23 +0300

    24h data

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use serde_derive::{Deserialize, Serialize};
 use csv;
 use csv::{WriterBuilder, Writer};
 use std::collections::BTreeMap;
+use std::ops::Bound::Included;
 use chrono::{DateTime, Utc, TimeZone};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -31,6 +32,11 @@ struct Output {
     RLH: f32,
     PRSS: f32,
     HPRAB: f32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Data_1 {
+    data: String,
 }
 
 fn main() {
@@ -87,7 +93,6 @@ fn main() {
 
     for record in sorted_records {
         let dt = Utc.datetime_from_str(&record.0, "%Y.%m.%d %H:%M:%S").unwrap();
-        println!("{} {:?}", dt.timestamp(), &record.1);
         sorted_records_datetime_numeric.insert(dt.timestamp(), record.1);
     }
 
@@ -107,11 +112,44 @@ fn main() {
             PRSS: full_record.PRSS,
             HPRAB: full_record.HPRAB,
         };
-        println!("{:?}", &full_record);
 
         sorted_records.insert(smallest, full_record);
         sorted_records_datetime_numeric.remove(&smallest);
     }
-    let mut sorted_records_numbered: BTreeMap<i64, FullRecord> = BTreeMap::new();
-    
+
+    let mut data_1: Vec<Vec<f32>> = Vec::new();
+
+    let length = sorted_records.len();
+    let mut i = 0;
+    for data in &sorted_records {
+        if length - 24 >= i {
+            let mut element = Vec::new();
+            for record in sorted_records.range((Included(*data.0), Included(data.0 + 3600*24+1))) {
+                element.push(record.1.TDRY);
+                element.push(record.1.RLH);
+                element.push(record.1.PRSS);
+                element.push(record.1.HPRAB);
+
+            }
+            data_1.push(element);
+            i+=1;
+        }
+    }
+    println!("{:?}", data_1);
+
+    let mut writer = WriterBuilder::new().from_path("output.csv").unwrap();
+    for record in data_1 {
+        let mut data = String::new();
+        for value in record {
+            data = data + &value.to_string() + " ";
+        }
+        data = data
+            .trim()
+            .replace(" ", ",");
+        let row = Data_1 {
+            data,
+        };
+        writer.serialize(row).unwrap();
+    }
+    writer.flush().unwrap();
 }
